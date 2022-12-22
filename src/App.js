ace1ec67bdee018c40ff96a230f677e119becdfb
Luana Lima <luana.arqeurb@gmail.com>
2022-12-22 14:36:15 +0000

    Added the HTML

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <Weather />
+      <div className="container">
+        <Weather />
+        <footer>
+          <a
+            href="https://github.com/luanacristinalima/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourse code
+          </a>
+          , by Luana Lima
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -3,74 +3,42 @@ import "./weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <div className="container">
-        <div className="card-wrapper">
-          <div className="row">
-            <div className="col">
-              <form className="d-flex">
-                <input
-                  className="form-control me-2"
-                  type="search"
-                  placeholder="Enter a city"
-                  aria-label="Search"
-                />
-                <button className="btn btn-outline-primary" type="submit">
-                  <i className="fa-solid fa-magnifying-glass"></i>
-                </button>
-              </form>
-            </div>
+      <form>
+        <div className="row">
+          <div className="col-11">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
           </div>
-          <header>
-            <h1>
-              <i className="fa-solid fa-location-dot"></i>
-              <span>Castelo Branco</span>
-            </h1>
-            <h2>Sun, Dec 18, 10:36</h2>
-          </header>
-          <div className="row">
-            <div className="col-2">
-              <img
-                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                alt=""
-                className="weather-icon"
-              />
-            </div>
-            <div className="col-8 weather-now">
-              <h3>Broken Clouds</h3>
-              <h4>
-                <span>10</span>
-                <span className="units">
-                  <a href="/" className="active">
-                    °C
-                  </a>
-                  |<a href="/">°F</a>
-                </span>
-              </h4>
-            </div>
-            <div className="col-2">
-              <p className="weather-parameters">Humidity:</p>
-              <p>
-                <i className="fa-solid fa-droplet icons"></i>
-                <span>78%</span>
-              </p>
-              <p className="weather-parameters">Wind:</p>
-              <p>
-                <i className="fa-solid fa-wind icons"></i>
-                <span>1.89km/h</span>
-              </p>
-            </div>
+          <div className="col-1">
+            <input type="submit" value="Search" className="btn btn-primary" />
           </div>
         </div>
-        <footer>
-          <a
-            href="https://github.com/luanacristinalima/weather-app-react"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Open-sourse code
-          </a>
-          , by Luana Lima
-        </footer>
+      </form>
+      <div className="row">
+        <div className="col-4">
+          <h1>Lisbon</h1>
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Partly cloudy"
+          />
+          <ul>
+            <li>14:21 lim</li>
+            <li>THU</li>
+            <li>Dec 22</li>
+          </ul>
+        </div>
+        <div className="col-4">
+          <h2>18 ºC|F</h2>
+          <h3>Partly cloudy</h3>
+          <ul>
+            <li>Humidity: 89%</li>
+            <li>Wind: 14 km/h</li>
+          </ul>
+        </div>
+        <div className="col-4">Forecast</div>
       </div>
     </div>
   );
