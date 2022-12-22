bc5863dbeca4237d67f585cd3cf1d239f6ac2f02
Luana Lima <luana.arqeurb@gmail.com>
2022-12-22 10:50:07 +0000

    Remove unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <Weather />
     </div>
   );
 }
-
-export default App;
